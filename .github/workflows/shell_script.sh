df1a1531fbd6fca333ec4a7f95b9a9060be34723
Nagesh-DevTools <nageshk@devtools.in>
2025-06-26 12:52:36 +0530

    added _

--- .github/workflows/shell_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo apt update
-sudo apt-get install -y cowsay
-mkdir -p ~/.cows
-curl -o ~/.cows/dragon.cow https://raw.githubusercontent.com/paulkaefer/cowsay-files/master/dragon.cow
